9762458d99a8bb53e4bec294b64d4215045b48e4
Gabriel Florit <gabrielflorit@gmail.com>
2016-02-01 18:10:17 -0500

    Add analytics click tags

--- src/js/util.js
+++ src/js/util.js
@@ -21,12 +21,28 @@
 		var facebookNode = document.getElementsByClassName(params.element.facebook);
 		for (var f = 0; f < facebookNode.length; f++) {
 			facebookNode[f].setAttribute('href', facebook);
+
+			facebookNode[f].addEventListener('click', function(e) {
+				var s=s_gi('nytbostonglobecom');
+				s.linkTrackVars='eVar15,channel,prop1';
+				s.linkTrackEvents='none';
+				s.tl(this,'o', 'BG Elections Share Tools – Facebook - Top');
+			});
+
 		}
 
 		var twitter = 'https://twitter.com/intent/tweet?text=' + encoded + '&via=BostonGlobe&url=' + encodeURI(href);
 		var twitterNode = document.getElementsByClassName(params.element.twitter);
 		for (var t = 0; t < twitterNode.length; t++) {
 			twitterNode[t].setAttribute('href', twitter);
+
+			twitterNode[t].addEventListener('click', function(e) {
+				var s=s_gi('nytbostonglobecom');
+				s.linkTrackVars='eVar15,channel,prop1';
+				s.linkTrackEvents='none';
+				s.tl(this,'o', 'BG Elections Share Tools – Twitter - Top');
+			});
+
 		}
 	};
 
